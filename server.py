4dae4824ac207ba707c7ec2f53157b682825c14a
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-03-23 03:58:28 -0400

    About to revamp dashboard code

--- server.py
+++ server.py
@@ -19,8 +19,34 @@ ROOT = os.path.dirname(__file__)
 
 logger = logging.getLogger("pc")
 pcs = set()
+curClient = None
 relay = MediaRelay()
 #  vid = cv2.VideoCapture(0)
+
+class NoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, track, transform):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.transform = transform
+
+    async def recv(self):
+
+        #  ret, frame2 = vid.read()
+
+        #  https://stackoverflow.com/questions/43665208/how-to-get-the-latest-frame-from-capture-device-camera-in-opencv
+
+        #  print("frame2: ")
+        #  print(frame2)
+
+        #  print(self.webcamPlayer.video)
+        frame = await self.track.recv()
+        return frame
 
 
 class VideoTransformTrack(MediaStreamTrack):
@@ -70,6 +96,13 @@ async def index(request):
     content = open(os.path.join(ROOT, "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
 
+async def dashboardpage(request):
+    content = open(os.path.join(ROOT, "views/dashboard.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+async def dashboardjs(request):
+    content = open(os.path.join(ROOT, "views/dashboard.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
 
 async def javascript(request):
     content = open(os.path.join(ROOT, "client.js"), "r").read()
@@ -83,6 +116,9 @@ async def offer(request):
     pc = RTCPeerConnection()
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
+    curClient = pc
+    print("PCS: ")
+    print(pcs)
 
     def log_info(msg, *args):
         logger.info(pc_id + " " + msg, *args)
@@ -101,7 +137,6 @@ async def offer(request):
         'framerate': '30',
         'video_size': '640x480'
     })
-
 
     @pc.on("datachannel")
     def on_datachannel(channel):
@@ -153,6 +188,81 @@ async def offer(request):
         ),
     )
 
+async def dashboardOffer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    print("length of pcs: ", len(pcs))
+    pc = list(pcs)[0]
+
+
+    def log_info(msg, *args):
+        logger.info(" " + msg, *args)
+
+    log_info("Created for %s", request.remote)
+
+    # prepare local media
+    player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+    if args.record_to:
+        recorder = MediaRecorder(args.record_to)
+    else:
+        recorder = MediaBlackhole()
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+
+        if track.kind == "audio":
+            pc.addTrack(player.audio)
+            recorder.addTrack(track)
+        elif track.kind == "video":
+
+            pc.addTrack(
+                NoTransformTrack(
+                    relay.subscribe(track), transform=params["video_transform"])
+            )
+            if args.record_to:
+                recorder.addTrack(relay.subscribe(track))
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            await recorder.stop()
+
+
+
+
+
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+    #  await recorder.start()
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
 
 async def on_shutdown(app):
     # close peer connections
@@ -161,71 +271,9 @@ async def on_shutdown(app):
     pcs.clear()
 
 
-#  async def renderOpencv(): 
-
-#      while True: 
-#          ret, frame = vid.read()
-
-#          # Display the resulting frame
-#          cv2.imshow('frame', frame)
-
-#          if cv2.waitKey(1) & 0xFF == ord('q'):
-#              break
-
-#          await asyncio.sleep(0.1)
-
-#  async def start_background_tasks(app): 
-#      app['opencv_renderer'] = asyncio.create_task(renderOpencv())
-
-#  async def cleanup_background_tasks(app): 
-#      app['opencv_renderer'].cancel()
-#      await app['opencv_renderer'] 
-
-
-#  async def main(): 
-#      parser = argparse.ArgumentParser(
-#          description="WebRTC audio / video / data-channels demo"
-#      )
-#      parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-#      parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-#      parser.add_argument(
-#          "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-#      )
-#      parser.add_argument(
-#          "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-#      )
-#      parser.add_argument("--record-to", help="Write received media to a file."),
-#      parser.add_argument("--verbose", "-v", action="count")
-
-#      args = parser.parse_args()
-
-#      if args.verbose:
-#          logging.basicConfig(level=logging.DEBUG)
-#      else:
-#          logging.basicConfig(level=logging.INFO)
-
-#      if args.cert_file:
-#          ssl_context = ssl.SSLContext()
-#          ssl_context.load_cert_chain(args.cert_file, args.key_file)
-#      else:
-#          ssl_context = None
-
-#      app = web.Application()
-#      app.on_startup.append(start_background_tasks)
-#      app.on_cleanup.append(cleanup_background_tasks)
-#      app.on_shutdown.append(on_shutdown)
-#      app.router.add_get("/", index)
-#      app.router.add_get("/client.js", javascript)
-#      app.router.add_post("/offer", offer)
-#      web.run_app(
-#          app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
-#      )
-
-
 
 
 if __name__ == "__main__":
-    #  asyncio.run(main())
     parser = argparse.ArgumentParser(
         description="WebRTC audio / video / data-channels demo"
     )
@@ -254,12 +302,13 @@ if __name__ == "__main__":
         ssl_context = None
 
     app = web.Application()
-    #  app.on_startup.append(start_background_tasks)
-    #  app.on_cleanup.append(cleanup_background_tasks)
     app.on_shutdown.append(on_shutdown)
     app.router.add_get("/", index)
+    app.router.add_get("/dashboard", dashboardpage)
+    app.router.add_get("/dashboard.js", dashboardjs)
     app.router.add_get("/client.js", javascript)
     app.router.add_post("/offer", offer)
+    app.router.add_post("/dashboardOffer", dashboardOffer)
     web.run_app(
         app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
     )
